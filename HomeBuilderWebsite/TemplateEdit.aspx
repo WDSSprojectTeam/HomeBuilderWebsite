1a4974b614a5a5c316fcf0a7964d7c6160cce967
Kathryn Massengill <kmassengill@ufl.edu>
2015-12-12 02:51:15 -0500

    changes

---
+++ HomeBuilderWebsite/TemplateEdit.aspx
@@ -0,0 +1,98 @@
+﻿<%@ Page Title="" Language="VB" MasterPageFile="~/Site.master" AutoEventWireup="false" CodeFile="TemplateEdit.aspx.vb" Inherits="TemplateEdit" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" Runat="Server">
+    <p>
+        <br />
+    </p>
+    <p>
+        <table>
+             <tr>
+             <td style="width: 632px">
+                 <asp:GridView ID="gvwEditTemplate" runat="server" AutoGenerateColumns="False" BackColor="White" BorderColor="#336666" BorderStyle="Double" BorderWidth="3px" CellPadding="4" GridLines="Horizontal" Width="630px">
+             <Columns>
+                 <asp:CommandField SelectText="Edit" ShowSelectButton="True" />
+                 <asp:BoundField DataField="Feature" HeaderText="Feature" />
+                 <asp:BoundField DataField="Name" HeaderText="Name">
+                 <ItemStyle Font-Bold="True" ForeColor="#000066" />
+                 </asp:BoundField>
+                 <asp:BoundField DataField="Description" HeaderText="Description" />
+                 <asp:BoundField DataField="Price" DataFormatString="{0:c}" HeaderText="Price" />
+             </Columns>
+                     <FooterStyle BackColor="White" ForeColor="#333333" />
+                     <HeaderStyle BackColor="#336666" Font-Bold="True" ForeColor="White" />
+                     <PagerStyle BackColor="#336666" ForeColor="White" HorizontalAlign="Center" />
+                     <RowStyle BackColor="White" ForeColor="#333333" />
+                     <SelectedRowStyle BackColor="#339966" Font-Bold="True" ForeColor="White" />
+                     <SortedAscendingCellStyle BackColor="#F7F7F7" />
+                     <SortedAscendingHeaderStyle BackColor="#487575" />
+                     <SortedDescendingCellStyle BackColor="#E5E5E5" />
+                     <SortedDescendingHeaderStyle BackColor="#275353" />
+         </asp:GridView>
+             </td>
+
+             <td style="border-style: none; border-width: thin; width: 375px" >
+                 <div style="text-align:left">
+                 <asp:Panel ID="Panel1" runat="server" BorderColor="Black" BorderStyle="Solid" Width="140px">
+                     <asp:Label ID="lblFeatureEdit" runat="server" align="left" Font-Bold="True" Font-Size="Large" Visible="False">Edit Feature:</asp:Label>
+                     <br />
+                     <br />
+                     <asp:Label ID="lblFeatureName" align="left" runat="server" Visible="False" Font-Bold="True" Font-Size="Large"></asp:Label>
+                 <br /> 
+    
+         
+         <asp:RadioButton ID="rdb1" alight="left" runat="server" Autopostback =" true" GroupName="1" Visible="False" />
+              
+      
+                 <br />
+              
+      
+        <asp:RadioButton ID="rdb2" runat="server" Autopostback=" true" GroupName="1" Visible="False" />
+                     
+     
+                 <br />
+                     
+     
+        <asp:RadioButton ID="rdb3" runat="server" Autopostback=" true" GroupName="1" Visible="False" />
+        
+                 <br />
+        
+        <asp:RadioButton ID="rdb4" runat="server" Autopostback=" true" GroupName="1" Visible="False" />
+        
+                 <br />
+        
+        <asp:RadioButton ID="rdb5" runat="server" Autopostback=" true" GroupName="1" Visible="False" />
+                 </asp:Panel>
+   </div>
+         <tr>
+             <td colspan="3"  >
+                 <br />
+                 <asp:Button ID="btnBack" runat="server"  Font-Names="Corbel" Text="Back" Font-Size="Larger" BackColor="#336666" Font-Bold="True" ForeColor="White" Width="200px" />
+                 &nbsp;
+                 <asp:Button ID="btnSave" runat="server"  Font-Names="Corbel" Text="Save My Changes" Font-Size="Larger" BackColor="#336666" Font-Bold="True" ForeColor="White" Width="200px" />
+               
+                 <asp:Button ID="btnEdit" runat="server" Text="Edit" />
+               
+             </td>
+         </tr>
+
+      </table>
+    <p>
+        &nbsp;</p>
+    <p>
+        &nbsp;</p>
+    <p>
+        &nbsp;</p>
+    <p>
+        &nbsp;</p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+</asp:Content>
+
---
+++ HomeBuilderWebsite/TemplateEdit.aspx.vb
@@ -0,0 +1,116 @@
+﻿
+
+Imports System.Data
+
+Partial Class TemplateEdit
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        rdb1.Visible = False
+        rdb2.Visible = False
+        rdb3.Visible = False
+        rdb4.Visible = False
+        rdb5.Visible = False
+        Dim myOptionList As List(Of Options) = Session("OptionSet")
+        Dim myChosenOptions As List(Of Integer)
+        Dim myTable As DataTable = Session("myTemplate")
+        Session("chosenTemplateFeatures") = myChosenOptions
+        Dim index As Integer = Session("whichTemplate")
+        Dim displayList As New DataTable
+        displayList.Columns.Add("Feature")
+        displayList.Columns.Add("Name")
+        displayList.Columns.Add("Description")
+        displayList.Columns.Add("Price")
+
+
+        For i = 4 To 13
+
+            myChosenOptions.Add(myTable.Rows(index).Item(i))
+
+        Next
+
+
+        For i = 0 To myOptionList.Count - 1
+            For j = 0 To myChosenOptions.Count - 1
+                If myOptionList.Item(i).getoptionID = myChosenOptions.Item(j) Then
+                    Dim list As New ArrayList
+                    displayList.Rows.Add()
+                    displayList.Rows(displayList.Rows.Count - 1)("Feature") = myOptionList.Item(i).getFeatureName
+                    displayList.Rows(displayList.Rows.Count - 1)("Name") = myOptionList.Item(i).getoptionname
+                    displayList.Rows(displayList.Rows.Count - 1)("Description") = myOptionList.Item(i).getoptiondescription
+                    displayList.Rows(displayList.Rows.Count - 1)("Price") = myOptionList.Item(i).getoptionprice
+
+                End If
+            Next
+
+        Next
+        gvwEditTemplate.DataSource = displayList
+        gvwEditTemplate.DataBind()
+    End Sub
+    Protected Sub gvwEditTemplate_RowCommand(sender As Object, e As GridViewCommandEventArgs) Handles gvwEditTemplate.RowCommand
+
+        Dim rowIndex As Integer = e.CommandArgument
+        Dim myOptionList As List(Of Options) = Session("OptionSet")
+        Dim myFeatureList As List(Of Feature) = Session("FeatureSet")
+        Dim optCount As Integer = 0
+        Dim myChosenOptions As List(Of Integer) = Session("chosenTemplateFeatures")
+
+        lblFeatureName.Visible = True
+        lblFeatureName.Text = myFeatureList.Item(rowIndex).Name
+
+        For i = 0 To myOptionList.Count - 1
+            If myOptionList.Item(i).getFeatureID = rowIndex + 1 Then
+                optCount += 1
+
+                If optCount = 1 Then
+                    rdb1.Text = myOptionList.Item(i).getoptionname
+                    rdb1.Visible = True
+                    If myOptionList.Item(i).getoptionID = myChosenOptions.Item(rowIndex) Then
+                        rdb1.Checked = True
+                    End If
+
+                ElseIf optCount = 2 Then
+                    rdb2.Text = myOptionList.Item(i).getoptionname
+                    rdb2.Visible = True
+                    If myOptionList.Item(i).getoptionID = myChosenOptions.Item(rowIndex) Then
+                        rdb2.Checked = True
+                    End If
+
+                ElseIf optCount = 3 Then
+                    rdb3.Text = myOptionList.Item(i).getoptionname
+                    rdb3.Visible = True
+                    If myOptionList.Item(i).getoptionID = myChosenOptions.Item(rowIndex) Then
+                        rdb3.Checked = True
+                    End If
+
+                ElseIf optCount = 4 Then
+                    rdb4.Text = myOptionList.Item(i).getoptionname
+                    rdb4.Visible = True
+                    If myOptionList.Item(i).getoptionID = myChosenOptions.Item(rowIndex) Then
+                        rdb4.Checked = True
+                    End If
+
+                ElseIf optCount = 5 Then
+                    rdb5.Text = myOptionList.Item(i).getoptionname
+                    rdb5.Visible = True
+                    If myOptionList.Item(i).getoptionID = myChosenOptions.Item(rowIndex) Then
+                        rdb5.Checked = True
+                    End If
+                End If
+
+
+
+
+
+            End If
+        Next
+
+
+
+
+    End Sub
+
+    Protected Sub btnEdit_Click(sender As Object, e As EventArgs) Handles btnEdit.Click
+
+    End Sub
+End Class
